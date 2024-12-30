3fddd52d7943020d1515651b1012a64a3ef65ad3
SwagyXD <mitraswat999@gmail.com>
2024-12-30 17:13:46 +0530

    Up

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -1,61 +1,37 @@
-import { useEffect, useRef } from "react";
-import anime from "animejs";
-import styles from "./hero.module.scss";
+import { motion, useAnimation } from "framer-motion";
 import { Reveal } from "@/components/utils/Reveal";
 import { DotGrid } from "./DotGrid";
+import styles from "./hero.module.scss";
+import { useEffect } from "react";
 
 export const Hero = () => {
-  const textWrapperRef = useRef<HTMLSpanElement>(null);
-  const lettersRef = useRef<HTMLSpanElement>(null);
+  const textControls = useAnimation();
 
   useEffect(() => {
-    if (!lettersRef.current || !textWrapperRef.current) return;
+    const animateText = async () => {
+      while (true) {
+        await textControls.start({
+          width: "100%",
+          transition: {
+            duration: 2,
+            ease: "linear",
+          },
+        });
 
-    const text = lettersRef.current.textContent || "";
-    lettersRef.current.innerHTML = text.replace(
-      /([^\x00-\x80]|\w)/g,
-      `<span class='${styles.letter}'>$&</span>`,
-    );
+        await new Promise((resolve) => setTimeout(resolve, 1500));
 
-    const textWidth = lettersRef.current.getBoundingClientRect().width;
+        await textControls.start({
+          width: "0%",
+          transition: {
+            duration: 1.5,
+            ease: "linear",
+          },
+        });
+      }
+    };
 
-    const timeline = anime.timeline({
-      loop: true,
-    });
-
-    timeline
-      .add({
-        targets: `.${styles.line}`,
-        scaleY: [0, 1],
-        opacity: [0.5, 1],
-        easing: "easeOutExpo",
-        duration: 700,
-      })
-      .add({
-        targets: `.${styles.line}`,
-        translateX: [0, textWidth + 10], // Add 10px padding
-        easing: "easeOutExpo",
-        duration: 700,
-        delay: 100,
-      })
-      .add({
-        targets: `.${styles.letter}`,
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 600,
-        offset: "-=775",
-        delay: (el, i) => 34 * (i + 1),
-      })
-      .add({
-        targets: [`.${styles.textWrapper}`],
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000,
-      });
-
-    return () => timeline.pause();
-  }, []);
+    animateText();
+  }, [textControls]);
 
   return (
     <section className={`section-wrapper ${styles.hero}`}>
@@ -68,37 +44,41 @@ export const Hero = () => {
           </Reveal>
           <Reveal>
             <h2 className={styles.subTitle}>
-              I&apos;m a{" "}
-              <span className={styles.textWrapper} ref={textWrapperRef}>
-                <span className={styles.line}></span>
-                <span className={styles.letters} ref={lettersRef}>
-                  Software Engineer
-                </span>
-              </span>
+              I&apos;m a <span>Software Engineer</span>
             </h2>
           </Reveal>
           <Reveal>
-            <>
-              <p className={styles.aboutCopy}>
-                Spent{" "}
-                <span className={styles.yellowtext}>
-                  1+ year building and scaling applications for some pretty cool
-                  companies and individuals.
-                </span>
-                <br />
-                <br />
-              </p>
-              <button
-                onClick={() =>
-                  document.getElementById("contact")?.scrollIntoView()
-                }
-                className={styles.connetbtn}
-              >
-                <span className={styles.textone}>Let&apos;s connect</span>
-                <span className={styles.texttwo}>Awsome</span>
-              </button>
-            </>
+            <p className={styles.aboutCopy}>
+              Spent{" "}
+              <span className={styles.yellowtext}>
+                1+ year building and scaling applications for some pretty cool
+                companies and individuals.
+              </span>
+              <br />
+              <br />
+            </p>
           </Reveal>
+          {/* <motion.div
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          <Image
+            className={styles.profile}
+            src={Profile}
+            priority
+            alt="Swatantra Mitra | Software Engineer"
+            width={250}
+            height={250}
+          />
+        </motion.div> */}
+          <button
+            onClick={() => document.getElementById("contact")?.scrollIntoView()}
+            className={styles.connetbtn}
+          >
+            <span className={styles.textone}>Let&apos;s connect</span>
+            <span className={styles.texttwo}>Awsome</span>
+          </button>
         </div>
       </div>
       <DotGrid />
